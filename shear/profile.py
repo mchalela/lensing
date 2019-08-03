f8514275445c2d4b55f16bfffecf227b4095be67
Martin <tinchochalela@gmail.com>
2019-08-03 16:23:56 -0300

    Read profile method

--- shear/profile.py
+++ shear/profile.py
@@ -17,23 +17,74 @@ Msun = 1.989e30 	# Solar mass (kg)
 
 class Profile(object):
 
-	def __init__(self, cat, rin_hMpc=0.1, rout_hMpc=10., bins=10, space='log', cosmo=cosmo, boot_flag=True, boot_n=100):
+	def __init__(self, cat=None, rin_hMpc=0.1, rout_hMpc=10., bins=10, space='log', cosmo=cosmo, boot_flag=True, boot_n=100):
 		
-		if isinstance(cat, pd.DataFrame):
-			cat = cat.to_numpy()
-
-		# Define some parameters...
-		self.set_Mpc_scale(dl=cat['DL'])
-		self.set_sigma_critic(dl=cat['DL'], ds=cat['DS'], dls=cat['DLS'])
-
-		# Compute distance and ellipticity components...
-		dist, theta = gentools.sphere_angular_vector(cat['RAJ2000'], cat['DECJ2000'],
-													cat['RA'], cat['DEC'], units='deg')
-		theta += 90. #np.pi/2.
-		dist_hMpc = dist*3600.*self.Mpc_scale*cosmo.h # distance to the lens in Mpc/h
-		et, ex = gentools.polar_rotation(cat['e1'], cat['e2'], np.deg2rad(theta))
-
 		# Create bins...
+		self.set_bins(rin_hMpc=rin_hMpc, rout_hMpc=rout_hMpc, bins=bins, space=space)
+		nbin = len(self.bins)-1
+		self.r_hMpc = 0.5 * (self.bins[:-1] + self.bins[1:])
+		self.shear = np.zeros(nbin, dtype=float)
+		self.cero = np.zeros(nbin, dtype=float)
+		self.shear_error = np.zeros(nbin, dtype=float)
+		self.cero_error = np.zeros(nbin, dtype=float)
+		self.stat_error = np.zeros(nbin, dtype=float)
+		self.N = np.zeros(nbin, int)
+
+		if cat is not None:
+			if isinstance(cat, pd.DataFrame):
+				cat = cat.to_numpy()
+
+			# Define some parameters...
+			Mpc_scale = self.set_Mpc_scale(dl=cat['DL'])
+			sigma_critic = self.set_sigma_critic(dl=cat['DL'], ds=cat['DS'], dls=cat['DLS'])
+
+			# Compute distance and ellipticity components...
+			dist, theta = gentools.sphere_angular_vector(cat['RAJ2000'], cat['DECJ2000'],
+														cat['RA'], cat['DEC'], units='deg')
+			theta += 90. #np.pi/2.
+			dist_hMpc = dist*3600.*Mpc_scale*cosmo.h # distance to the lens in Mpc/h
+			et, ex = gentools.polar_rotation(cat['e1'], cat['e2'], np.deg2rad(theta))
+
+			digit = np.digitize(dist_hMpc, bins=self.bins)-1
+			m_cal = np.ones(nbin, float)
+		
+			for i in range(nbin):
+				mask = digit==i
+				self.N[i] = mask.sum()
+				if self.N[i]==0: continue
+				weight = cat['weight'][mask]/sigma_critic[mask]**2
+				m_cal[i] = 1 + np.average(cat['m'][mask], weights=weight)
+
+				self.shear[i] = np.average(et[mask]*sigma_critic[mask], weights=weight) / m_cal[i]
+				self.cero[i]  = np.average(ex[mask]*sigma_critic[mask], weights=weight) / m_cal[i]
+
+				stat_error_num = np.sum( (0.25*weight*sigma_critic[mask])**2 )
+				stat_error_den = weight.sum()**2
+				self.stat_error[i] = np.sqrt(stat_error_num/stat_error_den) / m_cal[i]
+
+				if boot_flag:
+					err_t, err_x = self._boot_error(et[mask]*sigma_critic[mask],
+													ex[mask]*sigma_critic[mask], 
+													weight, boot_n)
+					self.shear_error[i] = err_t  / m_cal[i]
+					self.cero_error[i] = err_x / m_cal[i]
+
+	def __getitem__(self, key):
+		return getattr(self, key)
+
+	def set_Mpc_scale(self, dl):
+		Mpc_scale = dl*np.deg2rad(1./3600.)
+		self.Mpc_scale_mean = Mpc_scale.mean()
+		return Mpc_scale
+
+	def set_sigma_critic(self, dl, ds, dls):
+		beta = dls/ds
+		sigma_critic = cvel**2/(4.*np.pi*G*(dl*1e6*pc)) * (1./beta) * (pc**2/Msun)
+		self.beta_mean = beta.mean()
+		self.sigma_critic = sigma_critic.mean()
+		return sigma_critic
+
+	def set_bins(self, rin_hMpc, rout_hMpc, bins, space):
 		if type(bins)==int:
 			if space=='log':
 				self.bins = np.geomspace(rin_hMpc, rout_hMpc, bins+1)
@@ -41,53 +92,8 @@ class Profile(object):
 				self.bins = np.linspace(rin_hMpc, rout_hMpc, bins+1)
 		else:
 			self.bins = bins
-			rin_hMpc  = self.bins[0]
-			rout_hMpc = self.bins[-1]
-
-		nbin = len(self.bins)-1
-		digit = np.digitize(dist_hMpc, bins=self.bins)-1
-	
-		self.r_hMpc = 0.5 * (self.bins[:-1] + self.bins[1:])
-		self.shear = np.zeros(nbin, dtype=float)
-		self.cero = np.zeros(nbin, dtype=float)
-		self.shear_error = np.zeros(nbin, dtype=float)
-		self.cero_error = np.zeros(nbin, dtype=float)
-		self.stat_error = np.zeros(nbin, dtype=float)
-
-		m_cal = np.ones(nbin, float)
-		self.N = np.zeros(nbin, int)
-
-		for i in range(nbin):
-			mask = digit==i
-			self.N[i] = mask.sum()
-			if self.N[i]==0: continue
-			weight = cat['weight'][mask]/self.sigma_critic[mask]**2
-			m_cal[i] = 1 + np.average(cat['m'][mask], weights=weight)
-
-			self.shear[i] = np.average(et[mask]*self.sigma_critic[mask], weights=weight) / m_cal[i]
-			self.cero[i]  = np.average(ex[mask]*self.sigma_critic[mask], weights=weight) / m_cal[i]
-
-			stat_error_num = np.sum( (0.25*weight*self.sigma_critic[mask])**2 )
-			stat_error_den = weight.sum()**2
-			self.stat_error[i] = np.sqrt(stat_error_num/stat_error_den) / m_cal[i]
-
-			if boot_flag:
-				err_t, err_x = self._boot_error(et[mask]*self.sigma_critic[mask],
-												ex[mask]*self.sigma_critic[mask], 
-												weight, boot_n)
-				self.shear_error[i] = err_t  / m_cal[i]
-				self.cero_error[i] = err_x / m_cal[i]
-
-	def __getitem__(self, key):
-		return getattr(self, key)
-
-	def set_Mpc_scale(self, dl):
-		self.Mpc_scale = dl*np.deg2rad(1./3600.)
-		return None
-
-	def set_sigma_critic(self, dl, ds, dls):
-		self.beta = dls/ds
-		self.sigma_critic = cvel**2/(4.*np.pi*G*(dl*1e6*pc)) * (1./self.beta) * (pc**2/Msun)
+		self.rin_hMpc  = self.bins[0]
+		self.rout_hMpc = self.bins[-1]
 		return None
 
 	def _boot_error(self, shear, cero, weight, nboot):
@@ -106,9 +112,11 @@ class Profile(object):
 		'''Add a header to lensing.shear.Profile output file
 		to know the sample parameters used to build it.
 		
-		 file: 	 (str) Name of output file
-		 header: (dic) Dictionary with parameter cuts. Optional.
+		 file: 	    (str) Name of output file
+		 header:    (dic) Dictionary with parameter cuts. Optional.
 	            Example: {'z_min':0.1, 'z_max':0.3, 'odds_min':0.5}
+	     colnames:  (bool) Flag to write columns names as first line.
+	     		If False, column names are commented.
 		'''
 
 		with open(file, 'a') as f:
@@ -123,8 +131,25 @@ class Profile(object):
 			f.write('# '+'-'*48+'\n')
 
 			C = ' ' if colnames else '# '
-			f.write(C+'r_hMpc, shear, shear_error, cero, cero_error, stat_error \n')
+			f.write(C+'r_hMpc  shear  shear_error  cero  cero_error  stat_error  N \n')
 			p = np.column_stack((self.r_hMpc, self.shear, self.shear_error,
-								 self.cero, self.cero_error, self.stat_error))
-			np.savetxt(f, p, fmt=['%12.6f']*6)		
+								 self.cero, self.cero_error, self.stat_error, self.N))
+			np.savetxt(f, p, fmt=['%12.6f']*6+['%8i'])		
 		return None
+
+	def read_profile(self, file, colnames=True):
+		''' Read profile written with write_to() method
+		'''
+
+		with open(file, 'r') as f:
+			pf = np.genfromtxt(f, dtype=None, names=colnames)
+
+		p = Profile()
+		p['r_hMpc'] = pf['r_hMpc']
+		p['shear'] = pf['shear']
+		p['cero'] = pf['cero']
+		p['shear_error'] = pf['shear_error']
+		p['cero_error'] = pf['cero_error']
+		p['stat_error'] = pf['stat_error']
+		p['N'] = pf['N']
+		return p
