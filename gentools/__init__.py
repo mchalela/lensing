a51ea59ecee4e1684b165601f76dcbe80f5fe313
Martin <tinchochalela@gmail.com>
2020-04-09 20:18:39 -0300

    Precomputed distances

--- gentools/__init__.py
+++ gentools/__init__.py
@@ -1,5 +1,6 @@
 
 from .distance import sphere_angular_separation, sphere_angular_vector
+from .distance import compute_lensing_distances, _precompute_lensing_distances
 from .units import Mpc2deg, deg2Mpc
 from .rotation import polar_rotation, equatorial_coordinates_rotation
 from .tools import classonly
