1c4425de27577d9f8a197278d2adb19763cdfd6f
Martin Chalela <tinchochalela@gmail.com>
2020-09-08 18:31:06 -0300

    import fix

--- LensCat/__init__.py
+++ LensCat/__init__.py
@@ -4,6 +4,7 @@ from .cs82 import CS82
 from .cfht import CFHT
 from .rcsl import RCSL
 
-from .main import ExpandedCatalog, CompressedCatalog, read_catalog
+from .main import ExpandedCatalog, CompressedCatalog
+from .main import read_catalog, cat_paths
 
 from .mask_fields import build_mask, infield
